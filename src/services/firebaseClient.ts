933424939eaa21b53437636932ee9ccf4114eb2f
Jewel Barman <jewelbarman998@gmail.com>
2026-04-25 23:53:12 +0530

    fix: bugfix for build

--- src/services/firebaseClient.ts
+++ src/services/firebaseClient.ts
@@ -36,6 +36,7 @@ export class FirebaseClient {
         storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
         messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
         appId: import.meta.env.VITE_FIREBASE_APP_ID,
+        measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
       });
 
       this.services = {
